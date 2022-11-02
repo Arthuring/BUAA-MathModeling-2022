10454396ee1c1be1661d1b10f38b5bac1df230a2
Arthuring <20373091@buaa.edu.cn>
2022-11-02 20:00:22 +0800

    finish hw3

---
+++ hw3/cvxpy_solution_3.py
@@ -0,0 +1,21 @@
+# -- coding: utf-8 --
+import cvxpy as cp
+
+x1 = cp.Variable(3, integer=True)
+x2 = cp.Variable(2, pos=True)
+x = cp.hstack([x1, x2])
+
+obj = cp.Minimize(20 * x[0] + 90 * x[1] + 400 * x[2] + 70 * x[3] + 30 * x[4])
+
+cons = [
+    x[0] + x[1] + x[4] >= 30.5,
+    5 * x[2] + x[3] >= 30,
+    3 * x[0] + 10 * x[2] <= 120,
+    3 * x[1] + 2 * x[3] + x[4] <= 48,
+    x[0] >= 0,x[1] >= 0, x[2] >=0
+]
+
+prob = cp.Problem(obj, cons)
+prob.solve(solver='ECOS_BB')
+print("最优值为： ", prob.value)
+print("最优解为： ", x.value)
